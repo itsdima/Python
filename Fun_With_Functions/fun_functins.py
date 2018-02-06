7fd64dd61f2e34a96650ab691b9be5a10325ec25
Eric Pricop <dimitripricop@yahoo.com>
2018-02-05 20:07:34 -0800

    .

---
+++ Fun_With_Functions/fun_functins.py
@@ -0,0 +1,19 @@
+# print each number 1-1000 and specify weather it is odd or even
+def odd_even():
+	for i in range(1, 2001):
+		num = "Number is"
+		odd = ""
+		if i % 2 == 0:
+			odd = "This number is even"
+		else:
+			odd = "This number is odd"
+		print num, i, odd
+odd_even()
+#multiply each value in a given list by the given number (5)
+def multiply(a, by):
+	for i in range(0, len(a)):
+		a[i] = a[i] * by
+	print a
+a = [2,4,10,16]	
+b = multiply(a, 5)
+print b
