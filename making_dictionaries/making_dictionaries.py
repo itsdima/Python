5dd0b9014e4ba0b2bfbe8003a873f334156f8f5d
Eric Pricop <dimitripricop@yahoo.com>
2018-02-06 07:48:46 -0800

    done

---
+++ making_dictionaries/making_dictionaries.py
@@ -0,0 +1,6 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+my_fave_animal = zip(name,favorite_animal)
+my_fave_animal_dict = dict(my_fave_animal)
+print my_fave_animal_dict
